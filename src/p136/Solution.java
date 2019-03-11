944ae920509a7a6ebebb5a027e4d6bc443875ae8
winteralone <winteralone@gmail.com>
2019-03-11 19:12:20 +0800

    add solution to #136

---
+++ src/p136/Solution.java
@@ -0,0 +1,11 @@
+package p136;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num: nums) {
+            result^= num;
+        }
+        return result;
+    }
+}
